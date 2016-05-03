787160eac782f4514fe82240fe7d5c90298a5ee5
Eric Pernia <ericpernia@hotmail.com>
2016-05-03 03:16:11 -0300

    add EDU-CIAA-NXO DigitalIO and Dealy examples, and docs in progress

---
+++ sapi/sapi_bm-examples/EDU-CIAA-NXP/ej4_sAPI_blinky_tecla/inc/sAPI_Config.h
@@ -0,0 +1,80 @@
+/* Copyright 2016, Eric Pernia.
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#ifndef _SAPI_CONFIG_H_
+#define _SAPI_CONFIG_H_
+/** \brief Bare Metal example header file
+ **
+ ** This is a mini example of the CIAA Firmware
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Baremetal Bare Metal example header file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * ENP          Eric Pernia
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20160426   v0.0.1   First version
+ */
+
+/*==================[inclusions]=============================================*/
+
+#include "sAPI_DataTypes.h"
+
+/*==================[macros]=================================================*/
+
+#define SAPI_USE_TICK_HOOK FALSE
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _SAPI_CONFIG_H_ */
---
+++ sapi/sapi_bm-examples/EDU-CIAA-NXP/ej4_sAPI_blinky_tecla/src/main.c
@@ -0,0 +1,156 @@
+/* Copyright 2016, Eric Pernia.
+ * All rights reserved.
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** \brief Main example source file
+ **
+ ** This is a mini example of the CIAA Firmware.
+ **
+ **/
+
+/** \addtogroup CIAA_Firmware CIAA Firmware
+ ** @{ */
+/** \addtogroup Examples CIAA Firmware Examples
+ ** @{ */
+/** \addtogroup Main example source file
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * ENP          Eric Pernia
+ *
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 2016-04-26   v0.0.1   First version
+ */
+
+/*==================[inclusions]=============================================*/
+
+#include "main.h"         /* <= own header */
+
+#include "sAPI.h"         /* <= sAPI header */
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/** \brief Main function
+ *
+ * This is the main entry point of the software.
+ *
+ * \returns 0
+ *
+ * \remarks This function never returns. Return value is only to avoid compiler
+ *          warnings or errors.
+ */
+
+ /* FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE RESET. */
+int main(void)
+{
+   /* ------------- INICIALIZACIONES ------------- */
+
+   /* Inicializar la placa */
+   boardConfig();
+
+   /* Inicializar el conteo de Ticks con resolución de 1ms */
+   tickConfig(1);
+
+   /* Inicializar DigitalIO */
+   digitalConfig( 0, INITIALIZE );
+
+   /* Configuración de pines de entrada para
+	   Teclas de la CIAA-NXP */
+   digitalConfig( TEC1, INPUT );
+   digitalConfig( TEC2, INPUT );
+   digitalConfig( TEC3, INPUT );
+   digitalConfig( TEC4, INPUT );
+
+   /* Configuración de pines de salida para
+	   Leds de la CIAA-NXP */
+   digitalConfig( LEDR, OUTPUT );
+   digitalConfig( LEDG, OUTPUT );
+   digitalConfig( LEDB, OUTPUT );
+   digitalConfig( LED1, OUTPUT );
+   digitalConfig( LED2, OUTPUT );
+   digitalConfig( LED3, OUTPUT );
+
+   /* Variable de Retardo no bloqueante */
+   delay_t delay;
+
+   /* Inicializar Retardo no bloqueante con tiempo en ms
+	   500 ms = 0,5 s */
+   delayConfig( &delay, 500 );
+
+	uint8_t led = OFF;
+
+	uint8_t valor = 0;
+
+   /* ------------- REPETIR POR SIEMPRE ------------- */
+	while(1) {
+
+		/* delayRead retorna TRUE cuando se cumple el tiempo de retardo */
+		if ( delayRead( &delay ) ){
+         if( !led )
+		      led = ON;
+         else
+            led = OFF;
+	      digitalWrite( LEDB, led );
+		}
+
+      valor = !digitalRead( TEC4 );
+		digitalWrite( LED3, valor );
+
+	}
+
+	/* NO DEBE LLEGAR NUNCA AQUI, debido a que a este
+	   programa no es llamado por ningun S.O. */
+	return 0 ;
+}
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
